a97ad2ac4f18d75879ca0a09cac71b4e8ba63c08
Thomas Müller <tmueller@cpan.org>
2020-10-26 22:48:55 +0100

    fix: add missing overload for pipe with 10 args

--- fns.ts
+++ fns.ts
@@ -117,6 +117,18 @@ export type PipeFn = {
     gh: (g: G) => H,
     hi: (h: H) => I,
   ): I;
+  <A, B, C, D, E, F, G, H, I, J>(
+    a: A,
+    ab: (a: A) => B,
+    bc: (b: B) => C,
+    cd: (c: C) => D,
+    de: (d: D) => E,
+    ef: (e: E) => F,
+    fg: (f: F) => G,
+    gh: (g: G) => H,
+    hi: (h: H) => I,
+    ij: (i: I) => J,
+  ): J;
   <A, B, C, D, E, F, G, H, I, J, K>(
     a: A,
     ab: (a: A) => B,
